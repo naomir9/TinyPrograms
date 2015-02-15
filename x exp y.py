197ea9b6e3dccd08362182c69d61175e3c04ecda
Naomi <emaillr9@gmail.com>
2015-02-15 13:37:22 +0000

    x Exponential y

---
+++ x exp y.py
@@ -0,0 +1,6 @@
+# For variables x and y, prints the result for x exp y.
+
+base = int(input("what base?"))
+power = int(input("what power?"))
+x = base**power
+print(" %d to the %d is %d" %(base, power, x))
