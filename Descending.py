dfb8770e71bad16f85ade3588830f15a69715926
Naomi <emaillr9@gmail.com>
2015-02-18 13:45:54 +0000

    Descending Numbers

--- Descending.py
+++ Descending.py
@@ -1,17 +1,17 @@
-# Prints consecutive numbers in descending order
+# Prints consecutive numbers in descending order.
 num = 25
 while num > 0:
     num = num - 1
     print (num)
 
-# Prints even numbers in descending order
+# Prints even numbers in descending order.
 num = 24
 while num > 0:
     num = num - 2
     print(num)
 
 
-# Prints odd numbers in descending order
+# Prints odd numbers in descending order.
 num = 25
 while num > 1:
     num = num - 2
